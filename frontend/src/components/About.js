e3c8e4f6ab075349a93e52b673be012e57ace148
Alex Bridgeman <alex@MacBookPro.hsd1.fl.comcast.net>
2025-01-26 16:52:13 -0500

    testing deploy

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className="container mt-3">
             <div className="bg-secondary p-2.5 rounded-md overflow-y-auto">
                 <h3 className="text-xl font-bold mb-2">💡 About</h3>
-                <p>Telimpromptu is a free online game where players create and present whacky mad-libs style news broadcasts!</p>
+                <p>Telimpromptu is a free multiplayer game where players create and present whacky mad-libs style news broadcasts</p>
                 <Link to='/script-writing' className="text-accent hover:text-yellow-500">Want to write your own script? Click here</Link>
             </div>
         </div>
